517ceba4ef5befaf186da85c5f006132d2013e5c
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-08-12 14:10:53 +0500

    add navbar component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,10 @@
 import { Fragment } from "react";
+import NavBar from "../components/NavBar/NavBar";
 
 function App() {
   return (
     <Fragment>
+      <NavBar/>
       <div>App</div>
     </Fragment>
   );
