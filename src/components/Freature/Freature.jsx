5a308a96410540f8e5b8a79e7feb091e5af7663d
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-09-12 16:53:24 +0500

    call api in Freature component

--- src/components/Freature/Freature.jsx
+++ src/components/Freature/Freature.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import { IoIosArrowRoundForward } from "react-icons/io";
 import "./Freature.css";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../../redux/action";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Freature = () => {
+  const dispatch = useDispatch();
+  const [products, setProducts] = useState(null);
+  const [products2, setProducts2] = useState(null);
+  const [products3, setProducts3] = useState(null);
+  const data = useSelector((state) => state?.products);
+
+  useEffect(() => {
+    if (data?.products?.length > 0) {
+      const index1 = Math.floor(Math.random() * data?.products?.length);
+      const index2 = Math.floor(Math.random() * data?.products?.length);
+      const index3 = Math.floor(Math.random() * data?.products?.length);
+
+      if (index2 > data?.products?.length) {
+        index2++;
+      }
+      console.log(index2);
+      if (index3 > data?.products?.length) {
+        index3 += 2;
+      }
+      const selectedProduct1 = data?.products[index1];
+      setProducts(selectedProduct1);
+      const selectedProduct2 = data?.products[index2];
+      setProducts2(selectedProduct2);
+      const selectedProduct3 = data?.products[index3];
+      setProducts3(selectedProduct3);
+    }
+  }, [data]);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
   return (
     <div>
       <div className="freature-main">
         <div className="freature-left">
           <div className="left-text">
-            <h4>The Best Place To Play</h4>
-            <h2>Xbox Consoles</h2>
-            <p>
-              Save Up to 50% on select Xbox games.Get 3 months of PC game pass
-              for $2 USD
-            </p>
+            <h4>The Best Place To Sale</h4>
+            <h2>{products?.title}</h2>
+            <p>{products?.description}</p>
             <button>
               SHOP NOW
               <span>
@@ -22,10 +51,10 @@ const Freature = () => {
             </button>
           </div>
           <div className="left-img">
-            <img src="./watch.png" alt="watch" />
+            <img src={products?.thumbnail} alt="watch" />
           </div>
           <div className="price">
-            <span>$200</span>
+            <span>${products?.price}</span>
           </div>
         </div>
         <div className="freature-right">
@@ -33,7 +62,7 @@ const Freature = () => {
             <div className="right-top-main">
               <div className="right-top-text">
                 <h3>Summer Sales</h3>
-                <h4>New Google Pixel 6 Pro</h4>
+                <h4>{products2?.title}</h4>
                 <button>
                   SHOP NOW
                   <span>
@@ -42,10 +71,10 @@ const Freature = () => {
                 </button>
               </div>
               <div className="right-top-img">
-                <img src="./watch.png" alt="watch" />
+                <img src={products2?.thumbnail} alt={products2?.title} />
 
                 <div className="disscount">
-                  <span>25% OFF</span>
+                  <p>{products?.discountPercentage}% OFF</p>
                 </div>
               </div>
             </div>
@@ -53,11 +82,11 @@ const Freature = () => {
           <div className="freatur-left-bottom">
             <div className="left-top-main">
               <div className="left-top-img">
-                <img src="./watch.png" alt="watch" />
+                <img src={products3?.thumbnail} alt={products3?.title} />
               </div>
               <div className="left-top-text">
-                <h3>Xiaomi FlipBuds Pro</h3>
-                <h4>$299 USD</h4>
+                <h3>{products3?.title}</h3>
+                <h4>${products3?.price}</h4>
                 <button>
                   SHOP NOW
                   <span>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Outlet } from "react-router";
 
 const App = () => {
-  console.log("FASCLAJB");
-
   return (
     <>
       <Outlet />
