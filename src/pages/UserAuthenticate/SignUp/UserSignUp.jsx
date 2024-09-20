5c175b5d58a58f4ce9fc8372fa3079969fefb1ef
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-09-20 12:49:30 +0500

    sign up complete

--- src/pages/UserAuthenticate/SignUp/UserSignUp.jsx
+++ src/pages/UserAuthenticate/SignUp/UserSignUp.jsx
@@ -1,48 +1,88 @@
-import { Field, Form, Formik } from "formik";
+import "./SignUp.css";
 import React from "react";
-
+import { FcGoogle } from "react-icons/fc";
+import { Field, Form, Formik } from "formik";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { TbBrandAppleFilled } from "react-icons/tb";
 const UserSignUp = () => {
   return (
     <>
       <div className="user-sign-up-main">
         <div className="user-sign-up-card">
-          <div className="user-sign-up-heading">
-            <h2>Sign up</h2>
-          </div>
           <div className="sign-up-form">
+            <div className="user-sign-up-heading">
+              <h2>Sign up</h2>
+            </div>
             <Formik>
               <Form>
                 <h6>Name</h6>
-                <Field type="text" name="name" placeholder="Your Name" />
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="Your Name"
+                  className="input-field"
+                />
                 <h6>Email</h6>
-                <Field type="text" name="name" placeholder="Your Email" />
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="Your Email"
+                  className="input-field"
+                />
                 <h6>Password</h6>
-                <Field type="text" name="name" placeholder="Your Password" />
+                <Field
+                  type="password"
+                  name="name"
+                  placeholder="Your Password"
+                  className="input-field"
+                />
                 <h6>Confrim Password</h6>
-                <Field type="text" name="name" placeholder="Confrim Password" />
-                <p>
+                <Field
+                  type="password"
+                  name="name"
+                  placeholder="Confrim Password"
+                  className="input-field"
+                />
+                <p className="checkbox">
                   <Field type="checkbox" />
-                  Are you agree to Clicon <span>Terms of Condition</span> and
-                  <span> Privacy Policy</span>.
+                  <p>
+                    Are you agree to Clicon <span>Terms of Condition</span> and
+                    <span> Privacy Policy</span>.
+                  </p>
                 </p>
                 <button>
-                  Sign up <sapn></sapn>
+                  Sign up{" "}
+                  <sapn>
+                    <FaArrowRightLong />
+                  </sapn>
                 </button>
               </Form>
             </Formik>
           </div>
           <div className="other-links">
+            <p>
+              Already Have An Account?{" "}
+              <a href="/sign-in">
+                <span>Sign In</span>
+              </a>
+            </p>
             <p>or</p>
 
             <div className="third-party-sign-up">
               <p>
-                Sign up with google <span></span>
+                Sign up with google{" "}
+                <span>
+                  <FcGoogle />
+                </span>
               </p>
             </div>
 
             <div className="third-party-sign-up">
               <p>
-                Sign up with apple <span></span>
+                Sign up with apple{" "}
+                <span>
+                  <TbBrandAppleFilled />
+                </span>
               </p>
             </div>
           </div>
