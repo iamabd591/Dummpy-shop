922693b0aefebac82061cb62c685ec0330f4b701
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-09-20 18:32:01 +0500

    add formik and validation in sign in form

--- src/pages/UserAuthenticate/SignIn/userSignIn.jsx
+++ src/pages/UserAuthenticate/SignIn/userSignIn.jsx
@@ -1,78 +1,142 @@
 import React from "react";
 import "./userSignIn.css";
+import { useFormik } from "formik";
 import { FaEye } from "react-icons/fa";
 import { FaApple } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import "react-toastify/dist/ReactToastify.css";
+import { SignInSchema } from "../VlidationSchema";
+import { toast, ToastContainer } from "react-toastify";
 import { LiaLongArrowAltRightSolid } from "react-icons/lia";
-const userSignIn = () => {
+
+const onSubmit = async (values, actions) => {
+  try {
+    console.log(values);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    toast.success("Submitted Successfully!", {
+      autoClose: 5000,
+      pauseOnHover: true,
+      bodyClassName: "toast-body",
+      className: "toast-success custom-toast-container",
+    });
+
+    actions.resetForm();
+  } catch (error) {
+    toast.error("Something Went Wrong!");
+  }
+};
+
+const UserSignIn = () => {
+  const {
+    values,
+    errors,
+    touched,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: SignInSchema,
+    onSubmit,
+  });
+
   return (
-    <>
-      <div className="userSignIn-main">
-        <div className="userSignIn-card">
-          <div className="sign-heading">
-            <h2>Sign In</h2>
-            <div className="sign-form">
-              <Formik>
-                <Form>
-                  <h4>Email Address</h4>
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Enter Your Email"
-                    className="input-field"
-                  />
-                  <h4>Password</h4>
-                  <Field
-                    type="Password"
-                    name="password"
-                    placeholder="Enter Your Password"
-                    className="input-field"
-                  />
-                  <p className="eye-icon">
-                    <FaEye />
-                  </p>
-                  <p className="forgot-password">
-                    <a href="./forgot-password/send-otp">Forgot Password</a>
-                  </p>
-                  <button className="sign-btn">
-                    <p>Sign In</p>{" "}
-                    <span>
-                      <LiaLongArrowAltRightSolid />
-                    </span>
-                  </button>
-                </Form>
-              </Formik>
+    <div className="userSignIn-main">
+      <div className="userSignIn-card">
+        <div className="sign-heading">
+          <h2>Sign In</h2>
+          <div className="sign-form">
+            <form autoComplete="off" onSubmit={handleSubmit}>
+              <h4>Email Address</h4>
+              <input
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values?.email}
+                id="email"
+                placeholder="Enter Your Email"
+                className="input-field"
+              />
+              <p
+                className={errors?.email || touched?.email ? "input-error" : ""}
+              >
+                {errors?.email || touched?.email || errors?.email}
+              </p>
+
+              <h4>Password</h4>
+              <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values?.password}
+                id="password"
+                placeholder="Enter Your Password"
+                className="input-field"
+              />
+              <p
+                className={
+                  errors?.password || touched?.password ? "input-error" : ""
+                }
+              >
+                {errors?.password || touched?.password || errors?.password}
+              </p>
+
+              <p className="eye-icon">
+                <FaEye />
+              </p>
+              <p className="forgot-password">
+                <a href="./forgot-password/send-otp">Forgot Password</a>
+              </p>
+
+              <button
+                disabled={isSubmitting}
+                className="sign-btn"
+                type="submit"
+              >
+                <p>Sign In</p>
+                <span>
+                  <LiaLongArrowAltRightSolid />
+                </span>
+              </button>
+            </form>
+          </div>
+
+          <div className="other-sign">
+            <p>or</p>
+            <div className="google-sign">
+              <a href="/sign-up">
+                <p>Create An Account</p>
+              </a>
             </div>
-            <div className="other-sign">
-              <p>or</p>
-              <div className="google-sign">
-                <a href="/sign-up">
-                  <p>Create An Account</p>
-                </a>
-              </div>
-              <div className="google-sign">
-                <p>
-                  Login with google
-                  <span>
-                    <FcGoogle />
-                  </span>
-                </p>
-              </div>
-              <div className="apple-sign">
-                <p>
-                  Login with apple
-                  <span>
-                    <FaApple />
-                  </span>
-                </p>
-              </div>
+            <div className="google-sign">
+              <p>
+                Login with google
+                <span>
+                  <FcGoogle />
+                </span>
+              </p>
+            </div>
+            <div className="apple-sign">
+              <p>
+                Login with apple
+                <span>
+                  <FaApple />
+                </span>
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </>
+      <ToastContainer />
+    </div>
   );
 };
 
-export default userSignIn;
+export default UserSignIn;
---
+++ src/pages/UserAuthenticate/VlidationSchema.jsx
@@ -0,0 +1,47 @@
+import * as yup from "yup";
+const passwordRegx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+export const SignInSchema = yup.object().shape({
+  email: yup.string().email("Please Enter Valid Email").required("Required"),
+  password: yup
+    .string()
+    .min(8)
+    .matches(passwordRegx, { message: "Please Enter Valid Password" })
+    .required("Required"),
+});
+
+export const SignUpSchema = yup.object().shape({
+  name: yup.string().required("Required"),
+  email: yup.string().email("Enter Valid Email").required("required"),
+  password: yup
+    .string()
+    .min(8)
+    .matches(passwordRegx, { message: "Please Enter Strong Password" })
+    .required("Required"),
+  confrimPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Password must be matched")
+    .required("Required"),
+});
+
+export const SendOtpSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter the vaild email")
+    .required("Required"),
+});
+
+export const VerifyOtpSchema = yup.object().shape({
+  number: yup.number("Please Valid OTP").required("Required"),
+});
+
+export const ResetPasswordSchema = yup.object().shape({
+  password: yup
+    .string()
+    .matches(passwordRegx, { message: "Please Enter Strong Password" })
+    .required("Required"),
+  confrimPasword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Password must be matched")
+    .required("Required"),
+});
